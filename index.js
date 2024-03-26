8927eeea0b60bef58152467b471fa4acef448ac1
StevanZivadinovic <stevanzivadinovic@outlook.com>
2024-03-26 19:04:53 +0100

    izmena

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const mysql = require("mysql");
 const app = express();
-const blogRouterAppMain = require('./../routes/blogsRoutes.js')
+const blogRouter = require('./routes/blogsRoutes.js')
 app.listen(3000);
 
 //register new engine
@@ -14,7 +14,7 @@ app.use(express.json());
 //     console.log('every time this middleware renders');
 //     next()//ako ne stavis next() ne cita kod dalje, blokira refresh
 // })
-const generateSessionId = require("./../functions/generateIDForLogedUsers.js");
+const generateSessionId = require("./functions/generateIDForLogedUsers.js");
 const db = mysql.createConnection({
   host: "localhost",
   user: "root",
@@ -22,13 +22,13 @@ const db = mysql.createConnection({
   database: "blogs",
 });
 
-app.post("/signup_zahtev", (req: { body: { title: any; snippet: any; blog_body: any; }; }, res: { send: (arg0: string) => void; }) => {
+app.post("/signup_zahtev", (req, res) => {
   const sql =
     "INSERT INTO blogovi (`id`,`title`, `snippet`, `blog_body`) VALUES (?)";
   const ID = generateSessionId();
   const values = [ID, req.body.title, req.body.snippet, req.body.blog_body];
 
-  db.query(sql, [values], (err: any, data: string) => {
+  db.query(sql, [values], (err, data) => {
     if (err) {
       console.log(err);
       return res.send("Error");
@@ -37,20 +37,19 @@ app.post("/signup_zahtev", (req: { body: { title: any; snippet: any; blog_body: 
   });
 });
 
-app.get("/", (req: any, res: { redirect: (arg0: string) => void; }) => {
+app.get("/", (req, res) => {
   res.redirect("/blogs");
 });
-app.get("/about", (req: any, res: { render: (arg0: string, arg1: { title: string; }) => void; }) => {
+app.get("/about", (req, res) => {
   res.render("about", { title: "About" });
 });
 
 //middlewares
-app.use((req: { db: any; }, res: any, next: () => void) => {
+app.use((req, res, next) => {
   req.db = db;
   next();
 });
-app.use(blogRouterAppMain)
-app.use((req: any, res: { status: (arg0: number) => { (): any; new(): any; render: { (arg0: string, arg1: { title: string; }): void; new(): any; }; }; }) => {
+app.use(blogRouter)
+app.use((req, res) => {
   res.status(404).render("404", { title: "Page not exist!" });
 });
-module.exports = app;
